6296e9a24dcac81be9ec840f7baaa17bd2360b09
steelrony <uchechukwuaronu@gmail.com>
2018-11-09 23:03:43 +0100

    improved on burgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxx';
 
-class BurgerBuilder extends Component {
+import Aux from '../../hoc/Auxx';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component { 
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
 
