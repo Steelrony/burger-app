94929c92a847d8fb60dec36afb9125b3ebeb2f5f
steelrony <uchechukwuaronu@gmail.com>
2018-11-29 22:12:15 +0100

    added Logo and Navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './NavigationItem.css';
+
+const navigationItem = (props) => (
+    <li className={classes.navigationItem}>
+        <a href="/">A Link</a>
+    </li>
+);
+
+export default navigationItem;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,7 +101,7 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary 
                         ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
+                        price={this.state.totalPrice.toFixed(2)}
                         purchaseCancelled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
